613f4b1138e125020b5c062a20e59e2be73628c3
ainulshahiraayu <shahiraayuwork@gmail.com>
2025-06-19 03:03:59 +0800

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -39,3 +39,17 @@ if (hamburger && mobileNav) {
     })
   );
 }
+
+// SMOOTH SCROLL UNTUK HEADER NAVIGATION
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+    const target = document.querySelector(this.getAttribute("href"));
+    if (target) {
+      target.scrollIntoView({
+        behavior: "smooth"
+      });
+    }
+  });
+});
+
